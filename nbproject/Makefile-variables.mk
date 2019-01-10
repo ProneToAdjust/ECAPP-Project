a98dd4b733b1cc7e96052bb4e01781b37d185096
Vandrande-Varg <41341852+Vandrande-Varg@users.noreply.github.com>
2019-01-10 14:37:37 +0800

    commit

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=ECAPP_Project.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/ECAPP_Project.X.production.hex
+CND_ARTIFACT_NAME_default=ECAPP-Project.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ECAPP-Project.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=ecappproject.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ecappproject.x.tar
+CND_PACKAGE_NAME_default=ecapp-project.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ecapp-project.tar
