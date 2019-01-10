c76a4dde44f3008b2f54de0da8860f61bf6782cd
Vandrande-Varg <41341852+Vandrande-Varg@users.noreply.github.com>
2019-01-10 14:37:54 +0800

    commit

--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/ECAPP_Project.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=ECAPP_Project.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=ecappproject.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/ECAPP-Project.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=ECAPP-Project.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=ecapp-project/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/ecappproject.x/bin
+makeDirectory ${TMPDIR}/ecapp-project/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/ecappproject.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/ecapp-project.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/ecappproject.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/ecapp-project.tar *
 checkReturnCode
 
 # Cleanup
